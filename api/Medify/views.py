67ea25a99ee7151aea58880591047df57ff1796c
iamsuvhro <suvhradip@gmail.com>
2022-04-25 23:47:11 +0530

    implementation of file upload apiview

--- api/Medify/views.py
+++ api/Medify/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+import logging
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from django.http import HttpRequest
+from rest_framework.response import Response
+from .models import Malaria
+from Medify.services.malaria_detection import MalariaDetectionServices
+from serializers import MalariaSerializer
 
-# Create your views here.
+
+# class MalariaViewSet(viewsets.ModelViewSet):
+#     queryset = Malaria.objects.all()
+#     serializer_class = MalariaSerializer
+
+@api_view(["POST"])
+def verifyMalariaDetection(request: HttpRequest) -> Response:
+    """
+    Method for run Malaria detection 
+    """
+    if request.method == "POST":
+        params = {
+            'filename': request.data['filename'],
+            'image': request.data['image'],
+        }
+        response = Malaria.objects.create(filename=params['filename'], image=params['image'])
+        return Response({True,'File upload successfully',response})
+    else:
+        return Response({False,"Error occur while uploading the image files"})
+
