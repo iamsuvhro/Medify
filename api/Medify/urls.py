1c88ec2ce6a98727be4720859b57347f763ddae3
iamsuvhro <suvhradip@gmail.com>
2022-04-26 00:16:34 +0530

    Api added in urls

---
+++ api/Medify/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('verifyMalariaDetection', views.verifyMalariaDetection, name="verifyMalariaDetection")
+]
