6d8fe7526388c5ba09c2eb8a5c5316b0eaf1191f
iamsuvhro <suvhradip@gmail.com>
2022-04-27 15:26:01 +0530

    Functional features added in API

--- api/Medify/views.py
+++ api/Medify/views.py
@@ -3,13 +3,12 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from django.http import HttpRequest
 from rest_framework.response import Response
+from yaml import load
 from .models import Malaria
 from Medify.services.malaria_detection import MalariaDetectionServices
 
 
-# class MalariaViewSet(viewsets.ModelViewSet):
-#     queryset = Malaria.objects.all()
-#     serializer_class = MalariaSerializer
+
 
 @api_view(["POST"])
 def verifyMalariaDetection(request: HttpRequest) -> Response:
@@ -19,11 +18,20 @@ def verifyMalariaDetection(request: HttpRequest) -> Response:
     if request.method == "POST":
         params = {
             'filename': request.POST.get('filename', request.data),
-            'image': request.FILES.get('image','image'),
+            'image': request.FILES.get('image',request.data),
         }
-        response = Malaria.objects.create(filename=params['filename'], image=params['image'])
-
-        return Response({True,'File upload successfully'})
+        query = Malaria.objects.create(filename=params['filename'], image=params['image'])
+        
+        malariaDetection = MalariaDetectionServices(str(params['filename']))
+        imageDataArray = malariaDetection.imgToarray()
+        model = malariaDetection.load_model_()
+        preds, pred_val = malariaDetection.translate_malaria(model["model"].predict_proba(imageDataArray))
+        payload = ({"empty": False, 
+                    "para":preds[0], 
+                    "unin":preds[1],
+                    "pred_val": pred_val})
+        
+        return Response({True,'File upload successfully',payload})
     else:
         return Response({False,"Error occur while uploading the image files"})
 
